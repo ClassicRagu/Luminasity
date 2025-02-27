8ffb3fec1f748fc541fbc89e005659f9d10e7fd5
ClassicRagu <megameowthethird@gmail.com>
2025-02-27 17:23:37 -0600

    Add all the rest of the standard objectives

--- Luminasity/MogTomes.cs
+++ Luminasity/MogTomes.cs
@@ -51,13 +51,63 @@ namespace Luminasity
         {
             switch (content.ContentType.Value.ContentType.Value.RowId)
             {
-                // Dungeons
-                case 2:
+                // Dungeons, Trials, Raids
+                case 2: case 4: case 5: case 6:
                     var meow = contentFinderCondition.First((x) => x.Content.GetValueOrDefault<InstanceContent>() != null && x.Content.RowId == content.Content0.Value.Content.RowId);
                     Console.Write(meow.Name);
                     Console.Write(": ");
-                    Console.Write(content.RewardCount0);
+                    if(content.Score1 > -1) {
+                        Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                        Console.Write(" " + content.Score2 + " - " + content.RewardCount1 + " |");
+                        Console.Write(" " + content.Score3 + " - " + content.RewardCount2);
+                    } else {
+                        Console.Write(content.RewardCount0);
+                    }
                     Console.WriteLine();
+                    break;
+                // Ocean Fishing
+                case 31:
+                    Console.Write("Ocean Fishing: ");
+                    Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                    Console.Write(" " + content.Score2 + " - " + content.RewardCount1 + " |");
+                    Console.Write(" " + content.Score3 + " - " + content.RewardCount2);
+                    Console.WriteLine();
+                    break;
+                // GATEs, sadly there are no clear links here so we have to do something nasty with the code
+                case 35:
+                    switch(content.Content0.Value.Content.RowId) {
+                        case 1:
+                            Console.Write("Cliffhanger: ");
+                            Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                            Console.Write(" " + content.Score2 + " - " + content.RewardCount1);
+                            Console.WriteLine();
+                            break;
+                        case 8:
+                            Console.Write("Slice is Right: ");
+                            Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                            Console.Write(" " + content.Score2 + " - " + content.RewardCount1 + " |");
+                            Console.Write(" " + content.Score3 + " - " + content.RewardCount2 + " |");
+                            Console.Write(" " + content.Score4 + " - " + content.RewardCount3 + " |");
+                            Console.Write(" " + content.Score5 + " - " + content.RewardCount4);
+                            Console.WriteLine();
+                            break;
+                        case 5:
+                            Console.Write("Any Way the Wind Blows: ");
+                            Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                            Console.Write(" " + content.Score2 + " - " + content.RewardCount1 + " |");
+                            Console.Write(" " + content.Score3 + " - " + content.RewardCount2);
+                            Console.WriteLine();
+                            break;
+                        case 6: case 7:
+                            Console.Write("Leap of Faith or Airforce One?: ");
+                            Console.Write(" " + content.Score1 + " - " + content.RewardCount0 + " |");
+                            Console.Write(" " + content.Score2 + " - " + content.RewardCount1 + " |");
+                            Console.Write(" " + content.Score3 + " - " + content.RewardCount2);
+                            Console.WriteLine();
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 default:
                     break;
