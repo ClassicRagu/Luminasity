f3515f2e93029e4e9fe5817f5bb7ef3e49ce610b
ClassicRagu <megameowthethird@gmail.com>
2025-02-27 17:59:00 -0600

    Start adding minimog objectives

--- Luminasity/MogTomes.cs
+++ Luminasity/MogTomes.cs
@@ -15,6 +15,7 @@ namespace Luminasity
             var csBonusmissions = gameData.GetSubrowExcelSheet<CSBonusMission>();
             var csBonusSeason = gameData.GetExcelSheet<CSBonusSeason>();
             var contentFinderCondition = gameData.GetExcelSheet<ContentFinderCondition>();
+            var items = gameData.GetExcelSheet<Item>();
 
             string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "mogtomes");
 
@@ -30,24 +31,28 @@ namespace Luminasity
                     Console.WriteLine("");
                     Console.WriteLine(bonus.Text1.Value.Text);
                     Console.WriteLine("");
+                    Console.WriteLine("Standard/Weekly Objectives: ");
                     foreach (CSBonusMission mission in csBonusmissions[bonus.Category0])
                     {
-                        ProcessMogTome(mission.Content0.Value, contentFinderCondition);
-                        /*if(mission.Content0.Value.ContentType.Value.ContentType.Value.RowId == 31){
-
-                        } else {
-                            Console.WriteLine(mission.Content0.Value.Content0.Value.UnlockQuest0.Value.Name);
-                        }*/
-
+                        ProcessMogTome(mission.Content0.Value, contentFinderCondition, items);
                     }
 
-                    //Console.Write(
+                    Console.WriteLine("");
+                    Console.WriteLine("Minimog Objectives: ");
+                    foreach (var mission in csBonusmissions[bonus.Category2].Select((mission, i) => new {i, mission}))
+                    {
+                        Console.WriteLine("Week " + (mission.i + 1) + ":");
+                        Console.Write("Option 1 - ");
+                        ProcessMogTome(mission.mission.Content0.Value, contentFinderCondition, items);
+                        Console.Write("Option 2 - ");
+                        ProcessMogTome(mission.mission.Content1.Value, contentFinderCondition, items);
+                        //ProcessMogTome(mission.Content0.Value, contentFinderCondition);
+                    }
                 }
-                //ProcessMogTome(map, gameData, directoryPath, fullImport);
             }
         }
 
-        private static void ProcessMogTome(CSBonusContent content, ExcelSheet<ContentFinderCondition> contentFinderCondition)
+        private static void ProcessMogTome(CSBonusContent content, ExcelSheet<ContentFinderCondition> contentFinderCondition, ExcelSheet<Item> items)
         {
             switch (content.ContentType.Value.ContentType.Value.RowId)
             {
@@ -108,6 +113,38 @@ namespace Luminasity
                         default:
                             break;
                     }
+                    break;
+                case 8:
+                    Console.WriteLine("FATEs");
+                    break;
+                case 36:
+                    Console.WriteLine("Island Sanctuary");
+                    break;
+                case 27:
+                    Console.WriteLine("Masked Carnivale");
+                    break;
+                case 34:
+                    var fishItem = content.Content0.Value.Content.GetValueOrDefault<FishParameter>()?.Item.RowId;
+                    if(fishItem != null){
+                        var fishName = items.GetRow(fishItem ?? 0);
+                        Console.Write("Catch Fish: " + fishName.Name);
+                        Console.WriteLine();
+                    }
+                    break;
+                case 32:
+                    Console.WriteLine("Triple Triad");
+                    break;
+                case 9:
+                    Console.WriteLine("Treasure Hunt");
+                    break;
+                case 26:
+                    Console.WriteLine("Eureka");
+                    break;
+                case 29:
+                    Console.WriteLine("Save the Queen");
+                    break;
+                case 33:
+                    Console.WriteLine("The Hunt");
                     break;
                 default:
                     break;
